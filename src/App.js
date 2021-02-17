f88a987074c7d6ec27697fea06016164acc88986
German Rodrigo <gerorean@gmail.com>
2021-02-16 20:54:38 -0500

    miniproyecto tasks usando json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
+//import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
+import tasks from './sample/tasks.json';
+
+console.log(tasks);
 
 
+class App extends Component {//props datos que un componente recibe atravez de un objeto, PROPIEDADES
+  
+  state = {
+    tasks: tasks
+  }
+  
+  
+  render(){
+    return <div>
+      { 30 / 5 }
+      { this.state.tasks.map(e=> <p key={e.id}>{e.title} - {e.description} - {e.done} - {e.id}</p>)}
+    </div>
 
+    //<h1>hola mundo</h1>
+  }
+}
+
+/*
 //Estado: datos internos que tiene un componente
+
 class Helloworld extends React.Component {//props datos que un componente recibe atravez de un objeto, PROPIEDADES
   //console.log(props);
 
@@ -49,15 +71,15 @@ class Helloworld extends React.Component {//props datos que un componente recibe
       </h1>
       )
     }
-    /*return (
-      <div id='hello'>
-        <h3>{this.props.subt}</h3>
-        Hello World and {this.props.mytext}
-      </div>
-    );*/
+    ///return (
+    ///  <div id='hello'>
+    ///    <h3>{this.props.subt}</h3>
+    ///    Hello World and {this.props.mytext}
+    ///  </div>
+    ///);
   }
 }
-
+*/
 
 /*
 //Estado: datos internos que tiene un componente
@@ -72,13 +94,14 @@ function Helloworld(props) {//props datos que un componente recibe atravez de un
 }
 */
 
-
+/*
 const App = () =>
 <div className="App">This is my component: 
   <Helloworld mytext=' Hello Fazt' subt='lorem ipsu'/>
   <Helloworld mytext=' HelloooWorld' subt='fghf2'/>
   <Helloworld mytext=' hello!' subt='3'/>
 </div>
+*/
 
 /*
 function App() {
@@ -96,6 +119,5 @@ export default App;
         
 //https://www.youtube.com/watch?v=zIY87vU33aA&t=4s
 
-/**
-
- */
+//tasks.json: Arreglo de multiples objetos, cada objeto será una tarea, cada tarea tendra un id, etc.. varias propiedades..
+//{this.state.tasks}
